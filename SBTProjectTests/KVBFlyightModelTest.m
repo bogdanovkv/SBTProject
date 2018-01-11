0db2e272f7082ecf18c84f59f478be665e779629
Константин Богданов <bogdakos@mail.ru>
2018-01-11 11:40:43 +0300

    Добавлен тестовыq метод  - (void)testArrayFromDictionariesWithjClassType

--- SBTProjectTests/KVBFlyightModelTest.m
+++ SBTProjectTests/KVBFlyightModelTest.m
@@ -4,17 +4,16 @@
 //
 //  Created by Константин Богданов on 10.01.2018.
 //
-#include <objc/objc.h>
-#include <objc/runtime.h>
+
+
 #import <XCTest/XCTest.h>
 #import <OCMock.h>
 #import <Expecta.h>
 #import "KVBFlyightModel.h"
 
+
 @interface KVBFlyightModel(XCTests)
 
-
-+ (NSArray<KVBFlyightModel*>*)arrayFromDictionaries:(NSDictionary*)flightsDictionary;
 
 - (instancetype)initWithDictionary:(NSDictionary*)flightDictionary;
 - (NSString*)validValueFromString:(NSString*)inputString;
@@ -123,6 +122,8 @@
 
 - (void)testArrayFromDictionariesToBeNil
 {
+    
+   
     NSDictionary *testDictionary = @{
                                      @0: @{
             @"price": @35443,
@@ -141,17 +142,53 @@
             @"expires_at": @"2015-01-08T18:38:45Z"
         }
     };
-    
-    
+
     NSArray<KVBFlyightModel*> *testArray = [KVBFlyightModel arrayFromDictionaries:testDictionary];
     
     expect(testArray.count).equal(@2);
-    
-    expect(testArray.firstObject).notTo.beNil();
-    expect(testArray.lastObject).notTo.beNil();    
+    expect([testArray.firstObject isKindOfClass:[KVBFlyightModel class]]).beTruthy();
+    expect([testArray.lastObject isKindOfClass:[KVBFlyightModel class]]).beTruthy();
 }
 
+- (void)testArrayFromDictionariesWithjClassType
+{
+    NSDictionary *testDictionary =    @{
+                                       @"HKT": @{
+            @"0": @{
+                @"price": @35443,
+                @"airline": @"UN",
+                @"flight_number": @571,
+                @"departure_at": @"2015-06-09T21:20:00Z",
+                @"return_at": @"2015-07-15T12:40:00Z",
+                @"expires_at": @"2015-01-08T18:30:40Z"
+            },
+            @"1": @{
+                @"price": @27506,
+                @"airline": @"CX",
+                @"flight_number": @204,
+                @"departure_at": @"2015-06-05T16:40:00Z",
+                @"return_at": @"2015-06-22T12:00:00Z",
+                @"expires_at": @"2015-01-08T18:38:45Z"
+            },
+            @"2": @{
+                @"price": @31914,
+                @"airline": @"AB",
+                @"flight_number": @8113,
+                @"departure_at": @"2015-06-12T13:45:00Z",
+                @"return_at": @"2015-06-24T20:30:00Z",
+                @"expires_at": @"2015-01-08T15:17:42Z"
+            }
+        }
+    };
+    
+    NSArray<KVBFlyightModel*> *testArray = [KVBFlyightModel arrayFromDictionariesWithjClassType:testDictionary];
+    
+    expect(testArray.count).equal(@3);
+    expect([testArray[0] isKindOfClass:[KVBFlyightModel class]]).beTruthy();
+    expect([testArray[1] isKindOfClass:[KVBFlyightModel class]]).beTruthy();
+    expect([testArray[2] isKindOfClass:[KVBFlyightModel class]]).beTruthy();
 
+}
 
 
 
